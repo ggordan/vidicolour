bef30fd87d19fad9b7add7a8f20cd7f670868b15
Gordan Grasarevic <me@ggordan.com>
2013-01-19 01:27:28 +0100

    added looping through videos, and extracting images every minute

--- vidicolour.py
+++ vidicolour.py
@@ -1,6 +1,8 @@
 import cv
 
-input = 'rt.mkv'
+jumping = False
+
+input = 'pir.mp4'
 while not input:
     input = raw_input("Enter the absolute location of the movie: ")
     try:
@@ -8,18 +10,56 @@ while not input:
     except IOError as e:
         input = None
 
+def imageScale(height, width):
+    '''
+    Scales the image based on the maxWidth and maxHeight
+    '''
+    maxWidth = 640.0
+    maxHeight = 480.0
 
+    if width >= height:
+        if width <= maxWidth and height <= maxHeight:
+            return tuple((width, height))
+
+        widthRatio = maxWidth / width
+        heightRatio = maxHeight / height
+    else:
+        if height <= maxHeight and width <= maxWidth:
+            return tuple((width, height))
+
+        widthRatio = maxWidth / width
+        heightRatio = maxHeight / height
+
+    return tuple((int(width*widthRatio), int(height*heightRatio)))
+
+# load video
 capture = cv.CaptureFromFile(input)
-img=cv.QueryFrame(capture)
-img = cv.CreateImage(img.width, img.height), cv.IPL_DEPTH_32F, 3)
-cv.SaveImage('0.png', img)
 
-i = 0
+currentFrame = cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT)
+cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES, 25.0)
+
+# testing frame jumping. Couldn't get CV_CAP_PROP_POS_FRAMES to work on
+# the videos i tried
+# not sure if related to this issue: http://code.opencv.org/issues/1419
+# nonetheless, in case it doesn't work, dirty fallback
+if currentFrame != cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT):
+    jumping = True
+    cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES, float(currentFrame))
+
+# get an image every minute
+fpm = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FPS) * 60)
+
+# first query
+img = cv.QueryFrame(capture)
+
+i = 1
 while img:
-    if i > 300:
-        exit(1)
-    cv.WaitKey(60)
+    if not jumping:
+        for j in range(fpm):
+            cv.QueryFrame(capture)
+    else:
+        cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES, fpm * i)
+
     img = cv.QueryFrame(capture)
-    img = cv.CreateImage(imageScale(img.height, img.width), cv.IPL_DEPTH_32F, 3)
     cv.SaveImage('%s.png' % i, img)
     i += 1
