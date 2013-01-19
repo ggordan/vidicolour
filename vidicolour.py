50873e21a61579b5f4205a20db6d3f244dd1f3c7
Gordan Grasarevic <me@ggordan.com>
2013-01-19 03:25:57 +0100

    added image creation based on most dominant colour

--- vidicolour.py
+++ vidicolour.py
@@ -1,8 +1,21 @@
+from posix import getcwd
 import cv
+import Image
+import scipy
+import scipy.misc
+import scipy.cluster
+from glob import glob
+from PIL import Image, ImageDraw
+from time import time
+
+start_time = time()
 
 jumping = False
+NUM_CLUSTERS = 20
+IMG_HEIGHT = 700
+COLOR_WIDTH = 100
 
-input = 'pir.mp4'
+input = None
 while not input:
     input = raw_input("Enter the absolute location of the movie: ")
     try:
@@ -46,20 +59,65 @@ if currentFrame != cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_COUNT):
     jumping = True
     cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES, float(currentFrame))
 
-# get an image every minute
-fpm = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FPS) * 60)
+# get an image every 5 minutes
+fpm = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FPS) * (60*5))
 
 # first query
 img = cv.QueryFrame(capture)
+#img = None
 
 i = 1
 while img:
     if not jumping:
+        # dirt hack @todo need to get cv.SetCaptureProperty to work
         for j in range(fpm):
             cv.QueryFrame(capture)
     else:
         cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_POS_FRAMES, fpm * i)
 
     img = cv.QueryFrame(capture)
-    cv.SaveImage('%s.png' % i, img)
-    i += 1
+    if img:
+        cv.SaveImage('sample_images/%s.png' % i, img)
+        cv.WaitKey(fpm)
+        i += 1
+
+
+colours = []
+# get all the image
+images = glob("%s/sample_images/*.png" % getcwd())
+# process images
+for image in images:
+    # open the image
+    im = Image.open(image)
+    im = im.resize((150, 150))
+    # get PIL image
+    ar = scipy.misc.fromimage(im)
+    shape = ar.shape
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2])
+
+    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
+
+    vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
+    counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
+
+    index_max = scipy.argmax(counts)                    # find most frequent
+    peak = codes[index_max]
+    colour = '#' + ''.join(chr(c) for c in peak).encode('hex')
+    colours.append(tuple(peak))
+
+# set the image size
+image_size = tuple((len(images) * COLOR_WIDTH, IMG_HEIGHT))
+# create a new image
+newImage = Image.new('RGBA', image_size, (0, 0, 0, 0))
+# create a new draw object
+draw = ImageDraw.Draw(newImage) # Create a draw object
+
+increment = len(colours) * COLOR_WIDTH - COLOR_WIDTH
+for colour in colours:
+    print colour
+    print increment
+    draw.rectangle((increment, 0, COLOR_WIDTH, IMG_HEIGHT), fill=colour)
+#    newImage.save('sadasd%s.png' % increment)
+    increment -= COLOR_WIDTH
+
+newImage.save('vidicolour.png')
